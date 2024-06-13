71df4ea2f22808e8a1c37b3c62bc95393ceb7859
rusliahmad <rusliahmad@live.com>
2024-06-13 23:03:01 +0700

    Initial creation

---
+++ src/main/java/com/example/itemstock/ItemStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.itemstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itemstock/ItemStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
